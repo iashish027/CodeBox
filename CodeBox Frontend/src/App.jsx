f56b784abd39b566bafd89e4785230d5c7d8299b
iashish027 <ashishch296@gmail.com>
2025-10-21 12:04:45 +0530

    Initial Commit - Project Setup

---
+++ CodeBox Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='main-container w-3/4 mx-auto h-screen bg-white'>
+    
+      
+    </div>
+  )
+}
+
+export default App
